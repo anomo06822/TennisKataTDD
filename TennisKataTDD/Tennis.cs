f3e92862f232dfce1f4e11b73b4b6997536fa113
Jarvis <anomo06822@gmail.com>
2019-09-28 14:10:11 +0800

    refactor : add scoreLookup and merge if

--- TennisKataTDD/Tennis.cs
+++ TennisKataTDD/Tennis.cs
@@ -1,21 +1,24 @@
-﻿namespace TennisKata
+﻿using System.Collections.Generic;
+
+namespace TennisKata
 {
     public class Tennis
     {
         private int _firstPlayerScoreTimes;
 
+        private readonly Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
+        {
+            {1, "Fifteen"},
+            {2, "Thirty" },
+        };
+
         public string GetScore()
         {
-            if (_firstPlayerScoreTimes == 2)
+            if (_firstPlayerScoreTimes == 2 || _firstPlayerScoreTimes == 1)
             {
-                return "Thirty Love";
+                return $"{_scoreLookup[_firstPlayerScoreTimes]} Love";
             }
-
-            if (_firstPlayerScoreTimes == 1)
-            {
-                return "Fifteen Love";
-            }
-
+            
             return "Love All";
         }
 
