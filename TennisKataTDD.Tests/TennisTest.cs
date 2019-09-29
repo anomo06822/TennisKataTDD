9e51a4fc9de26834be0204dfde6280a6102ff2e6
Jarvis <anomo06822@gmail.com>
2019-09-29 19:19:48 +0800

    add 15-0 case

---
+++ TennisKataTDD.Tests/TennisTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace TennisKataTDD.Tests
+{
+    public class TennisTest
+    {
+        private Tennis _tennis;
+
+        public TennisTest()
+        {
+            _tennis = new Tennis();
+        }
+
+        [Fact]
+        public void Love_All()
+        {
+            ThenScoreShouldBe("Love All");
+        }
+
+        [Fact]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerTimes();
+            ThenScoreShouldBe("Fifteen Love");
+        }
+
+        private void GivenFirstPlayerTimes()
+        {
+            _tennis.FirstPlayerTimes();
+        }
+
+        private void ThenScoreShouldBe(string score)
+        {
+            _tennis.GetScore().Should().Be(score);
+        }
+    }
+}
---
+++ TennisKataTDD/Tennis.cs
@@ -0,0 +1,22 @@
+﻿namespace TennisKataTDD
+{
+    public class Tennis
+    {
+        private int _firstPlayerTimes;
+
+        public string GetScore()
+        {
+            if (_firstPlayerTimes == 1)
+            {
+                return "Fifteen Love";
+            }
+
+            return "Love All";
+        }
+
+        public void FirstPlayerTimes()
+        {
+            _firstPlayerTimes++;
+        }
+    }
+}
