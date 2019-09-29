165bff4e1e39bd8008f228fab63d4dd6c86fc392
Jarvis <anomo06822@gmail.com>
2019-09-29 13:48:01 +0800

    feat : add Fifteen Love case

---
+++ TennisKataTDD.Tests/TennisTest.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace TennisKataTDD.Tests
+{
+    public class TennisTest
+    {
+        private readonly Tennis _tennis;
+
+        public TennisTest()
+        {
+            _tennis = new Tennis();
+        }
+
+        [Fact]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [Fact]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerTimes();
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        private void GivenFirstPlayerTimes()
+        {
+            _tennis.FirstPlayerTimes();
+        }
+
+        private AndConstraint<StringAssertions> ScoreShouldBe(string score)
+        {
+            return _tennis.GetScore().Should().Be(score);
+        }
+    }
+}
