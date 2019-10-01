8a8c36a467015d9178daf2b873e7524bfbc9f895
Jarvis <anomo06822@gmail.com>
2019-10-01 23:53:58 +0800

    refactor

--- TennisKataTDD/Tennis.cs
+++ TennisKataTDD/Tennis.cs
@@ -1,19 +1,25 @@
-﻿namespace TennisKataTDD
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace TennisKataTDD
 {
     public class Tennis
     {
         private int _firstPlayerTimes;
 
+        private Dictionary<int, string> _lookupScore = new Dictionary<int, string>()
+        {
+            {1, "Fifteen" },
+            {2, "Thirty" },
+        };
+
         public string GetStore()
         {
-            if (_firstPlayerTimes == 2)
+            if (_firstPlayerTimes > 0)
             {
-                return "Thirty Love";
+                return $"{_lookupScore[_firstPlayerTimes]} Love";
             }
-            if (_firstPlayerTimes == 1)
-            {
-                return "Fifteen Love";
-            }
+
             return "Love All";
         }
 
