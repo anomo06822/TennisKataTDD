655f6c45f8db1422d9bfbb5a1404ea3ef839bbdf
Jarvis <anomo06822@gmail.com>
2019-10-01 23:44:18 +0800

    Feat : add 0-0 case

---
+++ TennisKataTDD.Tests/TennisTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace TennisKataTDD.Tests
+{
+    public class TennisTest
+    {
+        private Tennis _tennis;
+
+        public TennisTest()
+        {
+            _tennis = new Tennis();
+        }
+
+        [Fact]
+        public void Love_All()
+        {
+            ThenScoreShouldBe("Love All");
+        }
+
+        private void ThenScoreShouldBe(string score)
+        {
+            _tennis.GetStore().Should().Be(score);
+        }
+    }
+}
---
+++ TennisKataTDD/Tennis.cs
@@ -0,0 +1,10 @@
+﻿namespace TennisKataTDD
+{
+    public class Tennis
+    {
+        public string GetStore()
+        {
+            return "Love All";
+        }
+    }
+}
