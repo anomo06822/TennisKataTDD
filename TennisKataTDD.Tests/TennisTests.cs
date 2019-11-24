4cb5fdacb0e57ef523ecffc6cd247b6e11d7fea5
Jarvis <anomo06822@gmail.com>
2019-11-24 20:33:53 +0800

    feat : add 0-0

---
+++ TennisKataTDD.Tests/TennisTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace TennisKataTDD.Tests
+{
+    public class TennisTests
+    {
+        [Fact]
+        public void Love_All()
+        {
+            var tennis = new Tennis();
+            var result = tennis.GetScore();
+            result.Should().Be("Love_All");
+        }
+    }
+}
