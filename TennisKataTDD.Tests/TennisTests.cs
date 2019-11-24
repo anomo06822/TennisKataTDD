6bcb5dc5c1ac6f61c56cbad42cbbc6b3eac604ec
Jarvis <anomo06822@gmail.com>
2019-11-24 18:19:35 +0800

    all case

--- TennisKataTDD.Tests/TennisTests.cs
+++ TennisKataTDD.Tests/TennisTests.cs
@@ -14,7 +14,7 @@ namespace TennisKataTDD.Tests
 
         public TennisTests()
         {
-            _tennis = new Tennis();
+            _tennis = new Tennis("FirstPlayerName", "SecondPlayerName");
         }
 
         [Fact]
@@ -26,8 +26,118 @@ namespace TennisKataTDD.Tests
         [Fact]
         public void Fifteen_Love()
         {
-            _tennis.FirstPlayerTimes();
+            GivenFirstPlayerTimes(1);
             ThenScoreShouldBe("Fifteen_Love");
+        }
+
+        [Fact]
+        public void Thirty_Love()
+        {
+            GivenFirstPlayerTimes(2);
+            ThenScoreShouldBe("Thirty_Love");
+        }
+
+        [Fact]
+        public void Forty_Love()
+        {
+            GivenFirstPlayerTimes(3);
+            ThenScoreShouldBe("Forty_Love");
+        }
+
+        [Fact]
+        public void Love_Fifteen()
+        {
+            GivenSecondPlayerTimes(1);
+            ThenScoreShouldBe("Love_Fifteen");
+        }
+
+        [Fact]
+        public void Love_Thirty()
+        {
+            GivenSecondPlayerTimes(2);
+            ThenScoreShouldBe("Love_Thirty");
+        }
+
+        [Fact]
+        public void Love_Forty()
+        {
+            GivenSecondPlayerTimes(3);
+            ThenScoreShouldBe("Love_Forty");
+        }
+
+        [Fact]
+        public void Fifteen_All()
+        {
+            GiveSameScore(1);
+            ThenScoreShouldBe("Fifteen_All");
+        }
+
+        [Fact]
+        public void Thirty_All()
+        {
+            GiveSameScore(2);
+            ThenScoreShouldBe("Thirty_All");
+        }
+
+        [Fact]
+        public void Deuce()
+        {
+            GiveSameScore(3);
+            ThenScoreShouldBe("Deuce");
+        }
+
+        [Fact]
+        public void FirstPayer_Adv()
+        {
+            GiveSameScore(3);
+            GivenFirstPlayerTimes(1);
+            ThenScoreShouldBe("FirstPlayerName Adv");
+        }
+
+        [Fact]
+        public void SecondPlayer_Adv()
+        {
+            GiveSameScore(3);
+            GivenSecondPlayerTimes(1);
+            ThenScoreShouldBe("SecondPlayerName Adv");
+        }
+
+        [Fact]
+        public void FirstPayer_Win()
+        {
+            GiveSameScore(3);
+            GivenFirstPlayerTimes(2);
+            ThenScoreShouldBe("FirstPlayerName Win");
+        }
+
+        [Fact]
+        public void SecondPlayer_Win()
+        {
+            GiveSameScore(3);
+            GivenSecondPlayerTimes(2);
+            ThenScoreShouldBe("SecondPlayerName Win");
+        }
+
+        private void GiveSameScore(int times)
+        {
+            GivenFirstPlayerTimes(times);
+            GivenSecondPlayerTimes(times);
+        }
+
+        private void GivenFirstPlayerTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerTimes();
+            }
+        }
+
+        private void GivenSecondPlayerTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.SecondPlayerTimes();
+            }
         }
 
         private void ThenScoreShouldBe(string loveAll)
