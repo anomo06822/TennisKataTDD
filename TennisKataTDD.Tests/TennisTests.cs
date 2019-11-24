ffbc8c3b94ff3d702d8609658b82fc5539726d9a
Jarvis <anomo06822@gmail.com>
2019-11-24 17:28:13 +0800

    refactor

--- TennisKataTDD.Tests/TennisTests.cs
+++ TennisKataTDD.Tests/TennisTests.cs
@@ -1,7 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
 namespace TennisKataTDD.Tests
 {
     public class TennisTests
     {
-        
+        private Tennis _tennis;
+
+        public TennisTests()
+        {
+            _tennis = new Tennis();
+        }
+
+        [Fact]
+        public void Love_All()
+        {
+            ThenScoreShouldBe("Love_All");
+        }
+
+        private void ThenScoreShouldBe(string loveAll)
+        {
+            _tennis.GetScore().Should().Be(loveAll);
+        }
     }
 }
