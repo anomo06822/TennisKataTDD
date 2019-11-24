445b877401597fa7732c983ae4d2760a4cfdbc18
Jarvis <anomo06822@gmail.com>
2019-11-24 20:24:30 +0800

    Finshed!!

---
+++ TennisKataTDD.Tests/TennisTests.cs
@@ -0,0 +1,143 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TennisKataTDD.Tests
+{
+    public class TennisTests
+    {
+        private Tennis _tennis;
+
+        public TennisTests()
+        {
+            _tennis = new Tennis("SecondPlayerName", "FirstPlayerName");
+        }
+
+        [Fact]
+        public void Love_All()
+        {
+            ThenScoreShouldBe("Love_All");
+        }
+
+        [Fact]
+        public void Fifteen_Love()
+        {
+            GivenFirstPlayerTimes(1);
+            ThenScoreShouldBe("Fifteen_Love");
+        }
+
+        [Fact]
+        public void Thirty_Love()
+        {
+            GivenFirstPlayerTimes(2);
+            ThenScoreShouldBe("Thirty_Love");
+        }
+
+        [Fact]
+        public void Forty_Love()
+        {
+            GivenFirstPlayerTimes(3);
+            ThenScoreShouldBe("Forty_Love");
+        }
+
+        [Fact]
+        public void Love_Fifteen()
+        {
+            GivenSecondPlayerTimes(1);
+            ThenScoreShouldBe("Love_Fifteen");
+        }
+
+        [Fact]
+        public void Love_Thirty()
+        {
+            GivenSecondPlayerTimes(2);
+            ThenScoreShouldBe("Love_Thirty");
+        }
+
+        [Fact]
+        public void Love_Forty()
+        {
+            GivenSecondPlayerTimes(3);
+            ThenScoreShouldBe("Love_Forty");
+        }
+
+        [Fact]
+        public void Fifteen_All()
+        {
+            GivenSameScore(1);
+            ThenScoreShouldBe("Fifteen_All");
+        }
+
+        [Fact]
+        public void Thirty_All()
+        {
+            GivenSameScore(2);
+            ThenScoreShouldBe("Thirty_All");
+        }
+
+        [Fact]
+        public void Deuce()
+        {
+            GivenSameScore(3);
+            ThenScoreShouldBe("Deuce");
+        }
+
+        [Fact]
+        public void FirstPlayerName_Adv()
+        {
+            GivenSameScore(3);
+            GivenFirstPlayerTimes(1);
+            ThenScoreShouldBe("FirstPlayerName_Adv");
+        }
+
+        [Fact]
+        public void SecondPlayerName_Adv()
+        {
+            GivenSameScore(3);
+            GivenSecondPlayerTimes(1);
+            ThenScoreShouldBe("SecondPlayerName_Adv");
+        }
+
+        [Fact]
+        public void FirstPlayerName_Win()
+        {
+            GivenSameScore(3);
+            GivenFirstPlayerTimes(2);
+            ThenScoreShouldBe("FirstPlayerName_Win");
+        }
+
+        [Fact]
+        public void SecondPlayerName_Win()
+        {
+            GivenSameScore(3);
+            GivenSecondPlayerTimes(2);
+            ThenScoreShouldBe("SecondPlayerName_Win");
+        }
+
+        private void GivenSameScore(int times)
+        {
+            GivenFirstPlayerTimes(times);
+            GivenSecondPlayerTimes(times);
+        }
+
+        private void GivenFirstPlayerTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.FirstPlayerTimes();
+            }
+        }
+
+        private void GivenSecondPlayerTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennis.SecondPlayerTimes();
+            }
+        }
+
+        private void ThenScoreShouldBe(string expected)
+        {
+            _tennis.GetScore().Should().Be(expected);
+        }
+    }
+}
---
+++ TennisKataTDD/Tennis.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisKataTDD
+{
+    public class Tennis 
+    {
+        private int _firstPlayerTimes;
+        private int _secondPlayerTimes;
+
+        private Dictionary<int, string> _lookupScore = new Dictionary<int, string>()
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"},
+        };
+
+        private string _secondPlayerName;
+        private string _firstPlayerName;
+
+        public Tennis(string secondPlayerName, string firstPlayerName)
+        {
+            _secondPlayerName = secondPlayerName;
+            _firstPlayerName = firstPlayerName;
+        }
+
+        public string GetScore()
+        {
+            if (IsAdvantage())
+            {
+                return GetAdvantageScore();
+            }
+
+            if (IsReadyForWin())
+            {
+                return GetWinScore();
+            }
+
+            if (IsDeuce())
+            {
+                return GetDeuce();
+            }
+
+            return IsSameScore() ? GetSameScore() : GetNormalScore();
+        }
+
+        private bool IsReadyForWin()
+        {
+            return Math.Abs(_firstPlayerTimes - _secondPlayerTimes) >= 2 && (_firstPlayerTimes >= 4 || _secondPlayerTimes >=4 );
+        }
+
+        private bool IsAdvantage()
+        {
+            return Math.Abs(_firstPlayerTimes - _secondPlayerTimes) == 1 && (_secondPlayerTimes >= 3 || _firstPlayerTimes >=3);
+        }
+
+        private string GetSameScore()
+        {
+            return $"{_lookupScore[_firstPlayerTimes]}_All";
+        }
+
+        private string GetNormalScore()
+        {
+            return $"{_lookupScore[_firstPlayerTimes]}_{_lookupScore[_secondPlayerTimes]}";
+        }
+
+        private bool IsSameScore()
+        {
+            return _firstPlayerTimes == _secondPlayerTimes;
+        }
+
+        private bool IsDeuce()
+        {
+            return IsSameScore() && _firstPlayerTimes >= 3;
+        }
+
+        private static string GetDeuce()
+        {
+            return "Deuce";
+        }
+
+        private string GetWinScore()
+        {
+            return $"{GetLeaderPlayerName()}_Win";
+        }
+
+        private string GetAdvantageScore()
+        {
+            return $"{GetLeaderPlayerName()}_Adv";
+        }
+
+        private string GetLeaderPlayerName()
+        {
+            return _firstPlayerTimes > _secondPlayerTimes ? _firstPlayerName : _secondPlayerName;
+        }
+
+        public void FirstPlayerTimes()
+        {
+            _firstPlayerTimes++;
+        }
+
+        public void SecondPlayerTimes()
+        {
+            _secondPlayerTimes++;
+        }
+    }
+}
